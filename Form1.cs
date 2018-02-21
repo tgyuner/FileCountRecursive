4c2e510ead642387333dedaf5e1cd8ed827c27a6
DESKTOP-6GQ38RC\Tugay <tgy.uner@gmail.com>
2018-02-21 15:16:58 +0300

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileCountRecursive
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Tugay ÜNER 2018 ©
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            üstPath.Add("D:\\animals");
+            int fileCount = DosyaSayısı(üstPath);
+            MessageBox.Show(fileCount.ToString());
+        }
+
+        List<string> üstPath = new List<string>();
+        List<string> astPath = new List<string>();
+        List<string> temp = new List<string>();
+
+        public int DosyaSayısı(List<string> üstPath)
+        {
+            astPath = new List<string>();
+            temp = new List<string>();
+
+            if (üstPath.Count == 0)
+            {
+                return listBox1.Items.Count;
+            }
+            else
+            {
+                foreach (var dosya in üstPath)
+                {
+                    DirectoryInfo di = new DirectoryInfo(dosya);
+                    DirectoryInfo[] klasorler = di.GetDirectories();
+                    foreach (DirectoryInfo klasor in klasorler)
+                    {
+                        temp.Add(klasor.FullName);
+                        listBox1.Items.Add(klasor.Name);
+                    }
+                }
+
+                astPath = temp;
+                üstPath = astPath;
+
+                return DosyaSayısı(üstPath);
+
+            }
+        }
+
+    }
+}
